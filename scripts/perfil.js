444adb4cca4a264cd5f5b8fa3cb01b848cec41d7
lucmachuca <luc.machuca@duocuc.cl>
2025-09-15 22:59:45 -0300

    Modificaciones en pagina "Perfil"

--- scripts/perfil.js
+++ scripts/perfil.js
@@ -1,15 +1,17 @@
 // Recuperar datos del usuario desde localStorage
-const nombre = localStorage.getItem('nombre');
-const correo = localStorage.getItem('correo');
-const fecha = localStorage.getItem('fechaRegistro');
+const nombre = localStorage.getItem("nombre");
+const correo = localStorage.getItem("correo");
+const fecha = localStorage.getItem("fechaRegistro");
 
 // Mostrar en el perfil
-document.getElementById('perfil-nombre').textContent = nombre || "Usuario invitado";
-document.getElementById('perfil-correo').textContent = correo || "No registrado";
-document.getElementById('perfil-fecha').textContent = fecha || "Sin datos";
+document.getElementById("perfil-nombre").textContent =
+  nombre || "Usuario invitado";
+document.getElementById("perfil-correo").textContent =
+  correo || "No registrado";
+document.getElementById("perfil-fecha").textContent = fecha || "Sin datos";
 
 // Botón cerrar sesión
-document.getElementById('cerrar-sesion').addEventListener('click', () => {
+document.getElementById("cerrar-sesion").addEventListener("click", () => {
   localStorage.clear(); // elimina todos los datos guardados
   alert("Has cerrado sesión.");
   window.location.href = "login.html"; // redirige al login
